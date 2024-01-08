54bdf5db4c6ac08298400738228499fbdd6e7c6e
suhyeoonn <suhyeon1740@gmail.com>
2024-01-09 05:11:32 +0900

    🎨 서버에서 문제 데이터를 받아 출력하는 구조로 변경

---
+++ src/lib/components/OptionItem.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let id: string;
+	export let option: string;
+</script>
+
+<div class="flex items-center">
+	<input {id} class="mr-2" type="radio" name="scientist" />
+	<label for={id} class="text-lg"> {option} </label>
+</div>
---
+++ src/routes/+page.server.ts
@@ -0,0 +1,11 @@
+export function load() {
+	return {
+		questions: [
+			{
+				question: 'Which of the following is the capital city of France?',
+				options: ['Albert Einstein', 'Isaac Newton', 'Alexander Fleming', 'Marie Curie'],
+				answerIndex: 2
+			}
+		]
+	};
+}
